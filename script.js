001f3aa3c2d8582ce933fa7237058583f2f853e8
giorgisilagadze <silagadze.gio123@gmail.com>
2023-02-19 15:44:29 +0400

    second commit

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let tip = 0;
 let result;
 let res_tip;
 let p_cant = document.getElementById("p-cant");
+let reset = document.getElementById("reset");
 
 
 for (let i = 0; i < buttons.length; i++) {
@@ -20,11 +21,13 @@ for (let i = 0; i < buttons.length; i++) {
         if (numb.value <= 0 || numb.value == "" || bill.value <= 0 || bill.value == "") {
             result = "$0.00";
             res_tip = "$0.00";
+            reset.style.background = "";
         } else {
             p_cant.style.display = "none";
             numb.style.border = "none";
-            result = (parseInt(bill.value) + (parseInt(bill.value) * (parseInt(tip) / 100.0))) / parseInt(numb.value);
-            res_tip = ((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(tip) / 100.0));
+            result = ((parseInt(bill.value) + (parseInt(bill.value) * (parseInt(tip) / 100.0))) / parseInt(numb.value)).toFixed(2);
+            res_tip = (((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(tip) / 100.0))).toFixed(2);
+            reset.style.background = "#26C2AE"
         }
 
         total.innerHTML = result;
@@ -36,11 +39,13 @@ bill.addEventListener("input", () => {
     if (numb.value <= 0 || numb.value == "" || bill.value <= 0 || bill.value == "") {
         result = "$0.00";
         res_tip = "$0.00";
+        reset.style.background = "";
     } else {
         p_cant.style.display = "none";
         numb.style.border = "none";
-        result = (parseInt(bill.value) + (parseInt(bill.value) * (parseInt(tip) / 100.0))) / parseInt(numb.value);
-        res_tip = ((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(tip) / 100.0));
+        result = ((parseInt(bill.value) + (parseInt(bill.value) * (parseInt(tip) / 100.0))) / parseInt(numb.value)).toFixed(2);
+        res_tip = (((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(tip) / 100.0))).toFixed(2);
+        reset.style.background = "#26C2AE";
     }
 
     total.innerHTML = result;
@@ -53,11 +58,13 @@ numb.addEventListener("input", () => {
         res_tip = "$0.00";
         numb.style.border = "solid 1px red";
         p_cant.style.display = "block";
+        reset.style.background = "";
     } else {
         p_cant.style.display = "none";
         numb.style.border = "none";
-        result = (parseInt(bill.value) + (parseInt(bill.value) * (parseInt(tip) / 100.0))) / parseInt(numb.value);
-        res_tip = ((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(tip) / 100.0));
+        result = ((parseInt(bill.value) + (parseInt(bill.value) * (parseInt(tip) / 100.0))) / parseInt(numb.value)).toFixed(2);
+        res_tip = (((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(tip) / 100.0))).toFixed(2);
+        reset.style.background = "#26C2AE"
     }
 
 
@@ -65,3 +72,37 @@ numb.addEventListener("input", () => {
     tip_per.innerHTML = res_tip;
 
 })
+
+custom.addEventListener("input", () => {
+    if ((custom.value <= 0 || custom.value == "") && (numb.value > 0 || numb.value != "" || bill.value > 0 || bill.value != "")) {
+        result = ((parseInt(bill.value) + (parseInt(bill.value) * (parseInt(tip) / 100.0))) / parseInt(numb.value)).toFixed(2);
+        res_tip = (((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(tip) / 100.0))).toFixed(2);
+        reset.style.background = "#26C2AE";
+    } else {
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].style.background = ""
+        }
+        result = ((parseInt(bill.value) + (parseInt(bill.value) * (parseInt(custom.value) / 100.0))) / parseInt(numb.value)).toFixed(2);
+        res_tip = (((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(custom.value) / 100.0))).toFixed(2)
+        reset.style.background = "#26C2AE";
+    }
+
+    total.innerHTML = result;
+    tip_per.innerHTML = res_tip;
+})
+
+reset.addEventListener("click", () => {
+    bill.value = "";
+    custom.value = "";
+    numb.value = "";
+    result = "$0.00";
+    res_tip = "$0.00";
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.background = ""
+    }
+    reset.style.background = "";
+
+    total.innerHTML = result;
+    tip_per.innerHTML = res_tip;
+})
+
