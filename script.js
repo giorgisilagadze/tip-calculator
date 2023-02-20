9be5c2f36dcf4868c5e34ae9cc790005626777a3
giorgisilagadze <silagadze.gio123@gmail.com>
2023-02-20 19:46:51 +0400

    done

--- script.js
+++ script.js
@@ -10,99 +10,115 @@ let res_tip;
 let p_cant = document.getElementById("p-cant");
 let reset = document.getElementById("reset");
 
+function upper_formula() {
+    return ((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(tip) / 100.0)).toFixed(2);
+
+}
+
+function lower_formula() {
+    return ((parseInt(bill.value) + ((parseInt(bill.value) * (parseInt(tip) / 100.0)))) / parseInt(numb.value)).toFixed(2);
+}
+
+function valid() {
+    if (bill.value <= 0 || bill.value == "" || numb.value <= 0 || numb.value == "") {
+        res_tip = "$0.00";
+        result = "$0.00";
+        reset.style.background = "";
+    } else {
+        res_tip = "$" + upper_formula();
+        result = "$" + lower_formula();
+        p_cant.style.display = "none";
+        numb.style.border = "none";
+        reset.style.background = "#26C2AE";
+    }
+
+    tip_per.innerHTML = res_tip;
+    total.innerHTML = result;
+
+}
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", (event) => {
         for (let i = 0; i < buttons.length; i++) {
-            buttons[i].style.background = ""
-        }
-        event.target.style.background = "#26C2AE";
-        tip = ((event.target.innerText).slice(0, (event.target.innerText).length - 1));
-        if (numb.value <= 0 || numb.value == "" || bill.value <= 0 || bill.value == "") {
-            result = "$0.00";
-            res_tip = "$0.00";
-            reset.style.background = "";
-        } else {
-            p_cant.style.display = "none";
-            numb.style.border = "none";
-            result = ((parseInt(bill.value) + (parseInt(bill.value) * (parseInt(tip) / 100.0))) / parseInt(numb.value)).toFixed(2);
-            res_tip = (((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(tip) / 100.0))).toFixed(2);
-            reset.style.background = "#26C2AE"
+            buttons[i].style.background = "";
         }
 
-        total.innerHTML = result;
-        tip_per.innerHTML = res_tip;
+        event.target.style.background = "#26C2AE";
+        tip = (event.target.innerText).slice(0, (event.target.innerText).length - 1);
+        valid();
     })
 }
 
 bill.addEventListener("input", () => {
-    if (numb.value <= 0 || numb.value == "" || bill.value <= 0 || bill.value == "") {
-        result = "$0.00";
-        res_tip = "$0.00";
-        reset.style.background = "";
-    } else {
-        p_cant.style.display = "none";
-        numb.style.border = "none";
-        result = ((parseInt(bill.value) + (parseInt(bill.value) * (parseInt(tip) / 100.0))) / parseInt(numb.value)).toFixed(2);
-        res_tip = (((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(tip) / 100.0))).toFixed(2);
-        reset.style.background = "#26C2AE";
-    }
-
-    total.innerHTML = result;
-    tip_per.innerHTML = res_tip;
-})
-
-numb.addEventListener("input", () => {
-    if (numb.value <= 0 || numb.value == "") {
-        result = "$0.00";
-        res_tip = "$0.00";
-        numb.style.border = "solid 1px red";
-        p_cant.style.display = "block";
-        reset.style.background = "";
-    } else {
-        p_cant.style.display = "none";
-        numb.style.border = "none";
-        result = ((parseInt(bill.value) + (parseInt(bill.value) * (parseInt(tip) / 100.0))) / parseInt(numb.value)).toFixed(2);
-        res_tip = (((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(tip) / 100.0))).toFixed(2);
-        reset.style.background = "#26C2AE"
-    }
-
-
-    total.innerHTML = result;
-    tip_per.innerHTML = res_tip;
-
+    valid();  
 })
 
 custom.addEventListener("input", () => {
-    if ((custom.value <= 0 || custom.value == "") && (numb.value > 0 || numb.value != "" || bill.value > 0 || bill.value != "")) {
-        result = ((parseInt(bill.value) + (parseInt(bill.value) * (parseInt(tip) / 100.0))) / parseInt(numb.value)).toFixed(2);
-        res_tip = (((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(tip) / 100.0))).toFixed(2);
+    if (custom.value == "") {
+        tip = 0;
+    } else {
+        tip = custom.value;
+    }
+    if (bill.value <= 0 || bill.value == "" || numb.value <= 0 || numb.value == "" || custom.value < 0) {
+        res_tip = "$0.00";
+        result = "$0.00";
+        reset.style.background = "";
+        for (let i = 0; i < buttons.length; i++) {
+            buttons[i].style.background = ""
+        }
+        p_cant.style.display = "none";
+        numb.style.border = "none";
         reset.style.background = "#26C2AE";
     } else {
         for (let i = 0; i < buttons.length; i++) {
             buttons[i].style.background = ""
         }
-        result = ((parseInt(bill.value) + (parseInt(bill.value) * (parseInt(custom.value) / 100.0))) / parseInt(numb.value)).toFixed(2);
-        res_tip = (((parseInt(bill.value) / parseInt(numb.value)) * (parseInt(custom.value) / 100.0))).toFixed(2)
+        res_tip = "$" + upper_formula();
+        result = "$" + lower_formula();
+        p_cant.style.display = "none";
+        numb.style.border = "none";
         reset.style.background = "#26C2AE";
     }
 
-    total.innerHTML = result;
     tip_per.innerHTML = res_tip;
+    total.innerHTML = result;
 })
 
+numb.addEventListener("input", () => {
+    if (numb.value == 0 && numb.value != "") {
+        res_tip = "$0.00";
+        result = "$0.00";
+        numb.style.border = "solid 1px red";
+        p_cant.style.display = "block";
+        reset.style.background = "";
+    } else if (numb.value < 0 || numb.value == "" || bill.value < 0 || bill.value == "") {
+        res_tip = "$0.00";
+        result = "$0.00";
+        reset.style.background = "";
+        p_cant.style.display = "none";
+        numb.style.border = "none";
+    } else {
+        res_tip = "$" + upper_formula();
+        result = "$" + lower_formula();
+        p_cant.style.display = "none";
+        numb.style.border = "none";
+        reset.style.background = "#26C2AE";
+    }
+
+    tip_per.innerHTML = res_tip;
+    total.innerHTML = result;
+})
+
+
 reset.addEventListener("click", () => {
+    tip_per.innerHTML = "$0.00";
+    total.innerHTML = "$0.00";
+    p_cant.style.display = "none";
+    numb.style.border = "none";
     bill.value = "";
-    custom.value = "";
     numb.value = "";
-    result = "$0.00";
-    res_tip = "$0.00";
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].style.background = ""
     }
-    reset.style.background = "";
-
-    total.innerHTML = result;
-    tip_per.innerHTML = res_tip;
+    reset.style.background = ""
 })
-
